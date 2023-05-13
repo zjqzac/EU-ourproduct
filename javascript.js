1c4b873b1af62591aea94770967ff20067ae361d
jianqiao zhao <zjqzac@gmail.com>
2023-05-14 00:00:08 +1000

    version4
    btn

--- javascript.js
+++ javascript.js
@@ -10,5 +10,19 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 });
 
+var player = new Clappr.Player({
+  source: "./video/DeveloperMobile.gif", 
+  parentId: "#player5",
+  width:"100%",
+  height:"250px"
+});
+
+var player2 = new Clappr.Player({
+  source: "./video/video2.mp4", 
+  parentId: "#player2",
+  width:"684px",
+  height:"437.76px",
+  mediacontrol:{seekbar:"#16697A"}
+});
 
 
